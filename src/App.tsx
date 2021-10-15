3bfc8d2db0a5187adbbde7cbf1143bdd29ebaff6
Smikhotur <smikhotur@gmail.com>
2021-10-15 12:42:26 +0300

    Add peer membership

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { Peer } from './components';
+
+export const App: FC = () => {
+  return (
+    <div className='App'>
+      <Peer />
+    </div>
+  );
+};
+
+export default App;
